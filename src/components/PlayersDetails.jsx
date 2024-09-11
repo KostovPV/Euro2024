341a992d742724bd3475301d8f3ebab6fd219a36
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-09-11 15:58:36 +0300

    Css edited

--- src/components/PlayersDetails.jsx
+++ src/components/PlayersDetails.jsx
@@ -3,16 +3,16 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { DataContext } from '../context/DataContext';
 
 const PlayerDetails = () => {
-    const { id , matchID} = useParams();
+    const { id, matchID } = useParams();
     const data = useContext(DataContext);
     const players = data['players.csv'];
     const records = data['records.csv'];
     const matches = data['matches.csv'];
     const navigate = useNavigate();
-    
-    const player = players.find(player=>player.ID === id);
+
+    const player = players.find(player => player.ID === id);
     console.log('player->', player);
-    
+
     const playerInfo = records
         ?.filter(record => record.PlayerID === player?.ID && record.MatchID === matchID)
         ?.shift();
@@ -30,15 +30,24 @@ const PlayerDetails = () => {
     };
 
     return (
-        <div className="player-details-box">
-           
-            <h2>Player Details</h2>
-            <p><strong>Name:</strong> {player?.FullName}</p>
-            <p><strong>Number:</strong> {player?.TeamNumber}</p>
-            <p><strong>Position:</strong> {player?.Position}</p>
-            <p><strong>Minutes Played:</strong> {calculateMinutesPlayed(playerInfo)}</p>
-            <button onClick={handleBackClick} className="btn">Back</button>
+        <div className="player-container">
+            <div className="player-details-box">
+
+                <h2>Player Details</h2>
+                <div className="image-keeper">
+                    <img
+                        src={player?.Position === "GK" ? "/assets/images/goal-keeper.jpg" : "/assets/images/player.jpg"}
+                        alt="player-sketch"
+                    />
+                </div>
+                <p><strong>Name:</strong> {player?.FullName}</p>
+                <p><strong>Number:</strong> {player?.TeamNumber}</p>
+                <p><strong>Position:</strong> {player?.Position}</p>
+                <p><strong>Minutes Played:</strong> {calculateMinutesPlayed(playerInfo)}</p>
+                <button onClick={handleBackClick} className="btn">Back</button>
+            </div>
         </div>
+
     );
 };
 
